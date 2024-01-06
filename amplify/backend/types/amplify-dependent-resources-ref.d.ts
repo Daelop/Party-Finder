443a9b0c7e7978c89b63b539dead55d8f3a5dfec
unknown <jackdoddpost@gmail.com>
2024-01-06 20:56:32 +1300

    amplify and next?

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "PartyFinder": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "DaeStore": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
